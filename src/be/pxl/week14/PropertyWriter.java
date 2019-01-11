1d057b97a4a25b0273214909b5792050a0affae1
Dries Thoelen <dries.thoelen@student.pxl.be>
2019-01-11 15:10:58 +0100

    Corrections

--- src/be/pxl/week14/PropertyWriter.java
+++ src/be/pxl/week14/PropertyWriter.java
@@ -3,20 +3,33 @@ package be.pxl.week14;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class PropertyWriter {
-    public static void write(List<Properties> propertiesList) {
-        try(BufferedWriter writer = Files.newBufferedWriter(Paths.get("data/output.csv"))) {
-            for (Properties properties:propertiesList) {
-                for (Property property:properties.getData()) {
-                    writer.write(String.format("%s,%s,%s,%s,%d,%d,%d,%s,%s,%s,%s,%s", property.getStreet(), property.getCity(), property.getZip(), property.getState(), property.getBeds(), property.getBaths(), property.getSq_ft(), property.getType(), property.getSaleDate().format(Properties.FORMAT), property.getPrice().toString().replace(",", "."), property.getLatitude().toString().replace(",", "."), property.getLongitude().toString().replace(",", ".")));
-                    writer.newLine();
-                }
+public class PropertyWriter extends Thread {
+
+    private Path outputFile;
+    private List<Property> propertiesList;
+
+    public PropertyWriter(Path outputFile, List<Property> propertiesList) {
+        this.outputFile = outputFile;
+        this.propertiesList = propertiesList;
+    }
+
+    public void write() {
+        try(BufferedWriter writer = Files.newBufferedWriter(outputFile)) {
+            for (Property property:propertiesList) {
+                writer.write(String.format("%s,%s,%s,%s,%d,%d,%d,%s,%s,%s,%s,%s", property.getStreet(), property.getCity(), property.getZip(), property.getState(), property.getBeds(), property.getBaths(), property.getSq_ft(), property.getType(), property.getSaleDate().format(Properties.FORMAT), property.getPrice().toString().replace(",", "."), property.getLatitude().toString().replace(",", "."), property.getLongitude().toString().replace(",", ".")));
+                writer.newLine();
             }
         } catch (IOException ioex) {
-
+            ioex.printStackTrace();
         }
+    }
+
+    @Override
+    public void run() {
+        write();
     }
 }
--- src/be/pxl/week14/RealEstateApp.java
+++ src/be/pxl/week14/RealEstateApp.java
@@ -9,42 +9,43 @@ import java.util.List;
 public class RealEstateApp {
     public static void main(String[] args) {
         Properties properties = new Properties();
-        List<Properties> propertiesList = new ArrayList<>();
+        List<Property> propertiesList = new ArrayList<>();
 
         properties.readProperties(Paths.get("data\\sacramentorealestatetransactions.csv"));
-        System.out.println("Properties with price above $700000");
+        System.out.println("Properties with price above $800000");
         System.out.println("----------------------------------");
-        Properties properties1 = properties.propertiesAbovePrice(700000);
+        Properties properties1 = properties.propertiesAbovePrice(800000);
         System.out.println(properties1);
-        propertiesList.add(properties1);
+        propertiesList.addAll(properties1.getData());
         System.out.println("----------------------------------");
         System.out.println();
-        System.out.println("Properties with ZIP code 95648");
+        System.out.println("Properties with ZIP code 95650");
         System.out.println("------------------------------");
-        Properties properties2 = properties.propertiesForZIPCode("95648");
+        Properties properties2 = properties.propertiesForZIPCode("95650");
         System.out.println(properties2);
-        propertiesList.add(properties2);
+        propertiesList.addAll(properties2.getData());
         System.out.println("------------------------------");
         System.out.println();
         System.out.println("Properties sold after 20 May 2008");
         System.out.println("---------------------------------");
         Properties properties3 = properties.propertiesSoldAfter(ZonedDateTime.of(2008, 5, 20, 0, 0, 0, 0, ZoneId.of("America/New_York")));
         System.out.println(properties3);
-        propertiesList.add(properties3);
+        propertiesList.addAll(properties3.getData());
         System.out.println("---------------------------------");
         System.out.println();
         System.out.println("Last 10 properties sold");
         System.out.println("-----------------------");
-        Properties properties4 = properties.lastPropertiesSold(10);
+        Properties properties4 = properties.lastPropertiesSold(5);
         System.out.println(properties4);
-        propertiesList.add(properties4);
+        propertiesList.addAll(properties4.getData());
         System.out.println("-----------------------");
         System.out.println();
         System.out.println("Cheapest property with min. 3 bedrooms, min. 1000 sqft area, located in Sacramento");
         System.out.println("----------------------------------------------------------------------------------");
         System.out.println(properties.findCheapest());
+        propertiesList.add(properties.findCheapest());
         System.out.println("----------------------------------------------------------------------------------");
 
-        PropertyWriter.write(propertiesList);
+        new PropertyWriter(Paths.get("data/output.csv"), propertiesList).start();
     }
 }
